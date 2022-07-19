ed017a019efeec595ea2e4065c4567569cb5975f
Pano Maria <pano.maria@gmail.com>
2022-07-19 17:25:59 +0200

    prepare app for exit code generation

--- carml-app/src/main/java/io/carml/CarmlJarApplication.java
+++ carml-app/src/main/java/io/carml/CarmlJarApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarmlJarApplication {
   public static void main(String... args) {
-    SpringApplication.run(CarmlJarApplication.class, args);
+    System.exit(SpringApplication.exit(SpringApplication.run(CarmlJarApplication.class, args)));
   }
 }
